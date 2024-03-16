e2e5b4d139ebc2fd5c586fa5d7ec56b3b240f867
kelvin <kelvin4stunt@gmail.com>
2024-03-16 08:47:34 +0100

    python

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Returns the sum of floats in a list."""
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    """Returns the sum of floats in a list."""
+    return sum(mxd_lst)
