ace795eab44e191fee59f897fc86f7f75fab795b
kelvin <kelvin4stunt@gmail.com>
2024-03-24 14:43:12 +0100

    asyncio

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
     Coroutine that collects 10 random numbers using
     async comprehension over async_generator.
 """
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Coroutine that collects 10 random numbers using
     async comprehension over async_generator.
