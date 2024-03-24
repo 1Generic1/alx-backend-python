491a7ae4edd735ccaba4b1893a06c2c123a85c55
kelvin <kelvin4stunt@gmail.com>
2024-03-24 14:31:59 +0100

    asyncio

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator coroutine that
     yields random numbers between 0 and 10.
