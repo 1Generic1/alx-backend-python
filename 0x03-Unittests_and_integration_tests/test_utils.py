d8064b4e17eef05f1b4b4ae2843f592b949c98b6
kelvin <kelvin4stunt@gmail.com>
2024-04-10 06:18:58 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,5 +17,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """ test module """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ test module """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"Key not found: {path[-1]}")
+
 if __name__ == '__main__':
     unittest.main()
